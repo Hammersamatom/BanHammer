7464244f2c6dc88e391f3d0cac8df37efea4fdb5
Hammersamatom <hammersamatom@gmail.com>
2021-09-22 20:10:03 -0400

    v0.2 Launch

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
-# BanHammer -  A mass-banning tool for Twitch
+# BanHammer v0.2 - A mass-banning tool for Twitch - Now bans at Twitch's speed limit!
 ## SPECIAL NOTE
 
 * Windows Defender and other Anti-Virus software ***WILL*** mark ```BanHammer.exe``` as a virus
 	* This project uses ```PyInstaller``` to make the executable file ```BanHammer.exe```
 	* In the ***INFINITE WISDOM*** of Microsoft and other Anti-Virus providers, they've marked every executable made with ```PyInstaller``` as a virus instead of improving their virus detection
   * This software is currently licensed with a standard GPLv3 license which is found in the file ```LICENSE.md``` on this GitHub page.
+
+## Changelog - Current: v0.2
+### v0.2 - 9/22/2021
+#### Added
+- Added a config setting to change how quickly the program bans
+- Added setting custom end points when banning lists
+- Added error checking for the starting point in the ```config.txt``` file
+- Added error handling for Broken Pipes
+- Added a dirty hack to reconnect when Twitch dilently disconnects the program
+
+#### Fixed
+- Fixed ```config.txt``` parsing to handle leading whitespace better
+- Fixed disconnect function
+- <span style="color:limegreen">Fixed slow ban-rate, it's now the maximum allowed by Twitch</span>
+
+### v0.1 - 9/21/2021
+- Initial release
 
 ## Donate here if you want
 [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/W7W06AY2V)
@@ -30,13 +47,21 @@
    USER=ThisIsYourModeratorUsername
    OAUTH=oauth:hafh80vesee5urltfzsxkpv67cgcjo
    BANLIST=list.txt
+   RATELIMIT=0.3025
    STARTPOINT=0
+   ENDPOINT=MAX
    ```
    ```
    NOTE 1: The OAuth token used in the example is fake, you can get a real one in the Requirements section
    
    NOTE 2: In the config above, list.txt can be replaced with the name of any text file that follows the
    requirements listed below
+   
+   NOTE 3: RATELIMIT= is the seconds between bans. If left empty it will default to 0.3025 seconds per ban
+   
+   NOTE 4: STARTPOINT= can be left empty, it will default to the start of the list
+   
+   NOTE 5: ENDPOINT= can be set to MAX or be left empty. If so, the program will run over the entire list
    ```
 
 4. You're also going to need a ```list.txt``` of people to ban, with one name per line
--- banner.py
+++ banner.py
@@ -14,13 +14,16 @@ CHAN = ""                                       # Channelname = #{Nickname}
 NICK = ""                                       # Nickname = Twitch username
 PASS = ""                                       # www.twitchapps.com/tmi/ will help to retrieve the required authkey
 BNLT = ""                                       # List of individuals to ban
+RLMT = 0.3025                                   # Ban speed of 0.3025 seconds by default
 SPNT = 0                                        # Where to start in the Banlist
+EPNT = "MAX"                                    # Where to end in the Banlist
 # --------------------------------------------- End Settings -------------------------------------------------------
 
 
 # --------------------------------------------- Start Global Variables ---------------------------------------------
 
 twitchCon = ""
+setEndPointToLength = True
 
 # --------------------------------------------- End Global Variables ----------------------------------------------
 
@@ -32,9 +35,11 @@ twitchCon = ""
 # "USER="
 # "OAUTH="
 # "BANLIST="
+# "RATELIMIT="
 # "STARTPOINT="
+# "ENDPOINT="
 def initConfig():
-    paramCheckList = ["CHANNEL=", "USER=", "OAUTH=", "BANLIST=", "STARTPOINT="]
+    paramCheckList = ["CHANNEL=", "USER=", "OAUTH=", "BANLIST=", "RATELIMIT=", "STARTPOINT=", "ENDPOINT="]
     parameters = []
 
     try:
@@ -50,32 +55,56 @@ def initConfig():
         input("Press Enter to exit...")
         sys.exit()
     for item in parameters:
-        if paramCheckList[0] in item:
+        if paramCheckList[0] in item: # CHANNEL=
             global CHAN
             CHAN = "#" + item.replace(paramCheckList[0], '')
             CHAN = CHAN.lstrip()
             CHAN = CHAN.rstrip()
-        if paramCheckList[1] in item:
+        if paramCheckList[1] in item: # USER=
             global NICK
             NICK = item.replace(paramCheckList[1], '')
             NICK = NICK.lstrip()
             NICK = NICK.rstrip()
-        if paramCheckList[2] in item:
+        if paramCheckList[2] in item: # OAUTH=
             global PASS
             PASS = item.replace(paramCheckList[2], '')
             PASS = PASS.lstrip()
             PASS = PASS.rstrip()
-        if paramCheckList[3] in item:
+        if paramCheckList[3] in item: # BANLIST=
             global BNLT
             BNLT = item.replace(paramCheckList[3], '')
             BNLT = BNLT.lstrip()
             BNLT = BNLT.rstrip()
-        if paramCheckList[4] in item:
+        if paramCheckList[4] in item: # RATELIMIT=
+            global RLMT
+            RLMT_DEFAULT = RLMT
+            RLMT = item.replace(paramCheckList[4], '')
+            RLMT = RLMT.lstrip()
+            RLMT = RLMT.rstrip()
+            if RLMT == "":
+                RLMT = RLMT_DEFAULT
+            else:
+                RLMT = float(RLMT)
+        if paramCheckList[5] in item: # STARTPOINT=
             global SPNT
-            SPNT = item.replace(paramCheckList[4], '')
+            SPNT = item.replace(paramCheckList[5], '')
             SPNT = SPNT.lstrip()
             SPNT = SPNT.rstrip()
-            SPNT = int(SPNT)
+            if SPNT == "":
+                SPNT = 0
+            else:
+                SPNT = int(SPNT)
+        if paramCheckList[6] in item: # ENDPOINT=
+            global EPNT
+            global setEndPointToLength
+            EPNT = item.replace(paramCheckList[6], '')
+            EPNT = EPNT.lstrip()
+            EPNT = EPNT.rstrip()
+            if EPNT == "MAX" or EPNT == "":
+                setEndPointToLength = True
+            else:
+                setEndPointToLength = False
+                EPNT = int(EPNT)
 
     
 # --------------------------------------------- End Init Function --------------------------------------------------
@@ -91,7 +120,7 @@ def send_message(chan, msg):
         twitchCon.send(bytes('PRIVMSG %s :%s\r\n' % (chan, msg), 'UTF-8'))
     except socket.error as error:
         if error.errno == errno.EPIPE:
-            print("Broken Pipe! Rate limit too high? Exiting to prevent timeout!")
+            print("Broken Pipe durring Message Send! Rate limit too high? Exiting to prevent timeout!")
             input("Press Enter to exit...")
             sys.exit()
 
@@ -109,7 +138,13 @@ def join_channel(chan):
 
 
 def part_channel(chan):
-    twitchCon.send(bytes('PART %s\r\n' % chan, 'UTF-8'))
+    try:
+        twitchCon.send(bytes('PART %s\r\n' % chan, 'UTF-8'))
+    except socket.error as error:
+        if error.errno == errno.EPIPE:
+            print("Broken Pipe during Channel Leave! Restart the program!")
+            input("Press Enter to exit...")
+            sys.exit()
 # --------------------------------------------- End Functions ------------------------------------------------------
 
 
@@ -162,11 +197,16 @@ def reconnect():
 
 def main():
     # Initialization phase
+    global SPNT
+    global EPNT
     initConfig()
 
-    # Ignore all of this. Initial setup.
+    ### Ignore all of this. Initial setup phase.
+
+    # Set recentException to False so a reconnect() isn't called early
     recentException = False
-    count = SPNT
+    
+    # Initialize lines list
     lines = []
     try:
         with open(BNLT, encoding='UTF-8') as file:
@@ -176,18 +216,44 @@ def main():
         input("Press Enter to exit...")
         sys.exit()
 
+
+    ### Error checking for list Start and End spots
+    if setEndPointToLength == True:
+        EPNT = len(lines)
+    elif setEndPointToLength == False:
+        if EPNT > len(lines):
+            print("ENDPOINT= is over the list length. Setting to the length of the list...")
+            EPNT = len(lines)
+        
+    if SPNT > EPNT:
+        print("STARTPOINT= is more than ENDPOINT=. Qutting...")
+        sys.exit()
+    elif SPNT < 0:
+        print("STARTPOINT= is less than 0. Setting it to 0...")
+        SPNT = 0
+
+    # Initialize the listCounter variable to SPNT, whether that be somewhere in the list, or set to 0
+    listCounter = SPNT
+
+
+    ### Connection phase
     # Initialize connection
     connect()
 
     # Set   data   variable to empty
     data = ""
 
+    ### Print stats phase
     # Print the number of lines detected
-    print("Number of people to ban: %s" % len(lines))
+    print("Number of people to ban: %s" % (EPNT - SPNT))
+    print("Bans per minute: %s" % (60/RLMT))
+    print("Total time: %s hours" % (((EPNT - SPNT) / (60/RLMT))/60))
 
     while True:
         try:
             try:
+                # Set non-blocking. Required in order to skip the receive stage,
+                # so banning can happen as fast as possible
                 twitchCon.setblocking(False)
                 data = twitchCon.recv(1024).decode('UTF-8')
                 twitchCon.setblocking(True)
@@ -215,13 +281,15 @@ def main():
                         message = get_message(line)
                         print(sender + ": " + message)
 
-            if count < len(lines):
-                print(("[" + str(count) + "] Sending ban for %s" % lines[count]).rstrip())
-                send_message(CHAN, "/ban %s" % lines[count])
-                count += 1
-                time.sleep(2/5)
+            if listCounter < EPNT:
+                print(("[" + str(listCounter) + "] Sending ban for %s" % lines[listCounter]).rstrip())
+                send_message(CHAN, "/ban %s" % lines[listCounter])
+                listCounter += 1
+                time.sleep(RLMT)
 
-            if count == len(lines):
+            if listCounter == EPNT:
+                print("End of list hit. Qutting...")
+                input("Press Enter to exit...")
                 sys.exit()
 
             data = ""
